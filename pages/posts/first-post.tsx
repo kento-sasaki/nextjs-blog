30e7a63a5abb41a515b2ae11f5d552e30b23d7f5
kento <kozukata1993@gmail.com>
2022-03-08 23:33:50 +0900

    metadata

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from 'next';
+import Script from 'next/script';
+import Head from 'next/head';
 import Link from 'next/link';
 
 const FirstPost: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      {/* <Script
+        src='https://connect.facebook.net/en_US/sdk.js'
+        strategy='lazyOnload'
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      /> */}
       <h1>First Post</h1>
       <h2>
         <Link href='/'>

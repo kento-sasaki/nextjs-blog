916b2c8c4872304ef6874f4c68defcb3bf7a8d62
kento <kozukata1993@gmail.com>
2022-03-20 01:02:48 +0900

    fix styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { FC } from 'react'
 
-import utilStyles from '../../styles/utils.module.css'
+import utilStyles from '../styles/utils.module.css'
 
 import styles from './layout.module.css'
 

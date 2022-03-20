71a58b794f8efc638b0c910ded4490e2e5be92e7
kento <kozukata1993@gmail.com>
2022-03-20 15:56:46 +0900

    feat: introduce storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,46 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'prettier',
-  ],
+  extends: ["eslint:recommended", "next/core-web-vitals", "plugin:@typescript-eslint/recommended", "plugin:import/recommended", "plugin:import/typescript", "prettier", "plugin:storybook/recommended"],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint', 'react-hooks', 'import'],
   settings: {
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+      '@typescript-eslint/parser': ['.ts', '.tsx']
     },
     'import/resolver': {
       typescript: {
-        alwaysTryTypes: true,
-      },
-    },
+        alwaysTryTypes: true
+      }
+    }
   },
   rules: {
     'import/no-unresolved': 'off',
     'import/first': 'error',
     'import/newline-after-import': 'error',
     'import/no-duplicates': 'error',
-    'import/order': [
-      'error',
-      { 'newlines-between': 'always', alphabetize: { order: 'asc' } },
-    ],
+    'import/order': ['error', {
+      'newlines-between': 'always',
+      alphabetize: {
+        order: 'asc'
+      }
+    }],
     'react-hooks/exhaustive-deps': 'error',
     'react/jsx-curly-brace-presence': 'error',
     '@typescript-eslint/no-empty-function': 'warn',
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: {
-          '{}': false,
-        },
-      },
-    ],
-    'react/self-closing-comp': [
-      'error',
-      {
-        component: true,
-        html: true,
-      },
-    ],
-  },
+    '@typescript-eslint/ban-types': ['error', {
+      types: {
+        '{}': false
+      }
+    }],
+    'react/self-closing-comp': ['error', {
+      component: true,
+      html: true
+    }]
+  }
 };

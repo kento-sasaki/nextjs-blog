5ef8e214a27b6dbd913346f2c015007a6d50ca3a
kento <kozukata1993@gmail.com>
2022-03-20 15:08:29 +0900

    fix page config

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,50 @@
+import type { NextPage, GetStaticProps } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { Date } from '../components/date'
+import { Layout, siteTitle } from '../components/layout'
+import { getSortedPostsData, MetaData } from '../lib/posts'
+import utilStyles from '../styles/utils.module.css'
+
+type Props = {
+  allPostsData: MetaData[]
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+const Home: NextPage<Props> = ({ allPostsData }) => {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export default Home
